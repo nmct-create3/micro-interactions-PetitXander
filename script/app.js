0387402ff44410477b0d10995075827ada94b182
PetitXander <55786870+PetitXander@users.noreply.github.com>
2020-10-07 08:55:02 +0200

    password

--- script/app.js
+++ script/app.js
@@ -1,13 +1,27 @@
-function handleFloatingLabel() {
-   
-}
-
 function handlePasswordSwitcher() {
     
+    let checkbox = document.querySelector(`.c-toggle-password__checkbox`);
+    let passwordInput = document.querySelector(`.c-toggle-password__input`);
+    checkbox.addEventListener(`change`, function(){
+
+        if(checkbox.checked){
+            passwordInput.type = "text";
+            
+            console.log("checked")
+        }
+        else{
+            passwordInput.type = "password";
+            console.log("unchecked")
+        }
+        
+
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
-    handleFloatingLabel();
+    // handleFloatingLabel();
     handlePasswordSwitcher();
 });
